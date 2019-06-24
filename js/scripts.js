0c6e7e75b475c0c577feb5507c2f7f0f5e5bc189
Celia Baptiste <cfbaptiste@gmail.com>
2019-06-23 19:54:04 -0700

    get code to give output [object HTMLDivElement]

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@
 // handles core functionality and computations of the application
 
 //
-var pingpong = function(number) {
-  if ((number % 3 === 0)) {
-    return true;
-  } else if ((number % 5 === 0))  {
-    return true;
-  } else if ((number % 15 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-
-
-};
+// var pingpong = function(number) {
+//   if ((number % 3 === 0)) {
+//     return true;
+//   } else if ((number % 5 === 0))  {
+//     return true;
+//   } else if ((number % 15 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//
+//
+// };
 
 
 //alert("Please enter a number greater than 0";)
@@ -25,25 +25,42 @@ var pingpong = function(number) {
 $(function()  {
   $("form#ping-pong").submit(function(event)  {
     event.preventDefault();
-    var number = parseInt($("#numberinput").val());
-    var result = pingpong(number);
+    var number = parseInt($("#numberInput").val());
+
+    // var result = pingpong(number);
     for (var startingnumber = 0; startingnumber <= number; startingnumber +=1)  {
       console.log(startingnumber);
     }
-    for (var ping = ; ) {
-      console.log("ping");
-    }
-    for (var pong = ;){
-      console.log("pong");
-    }
+    console.log("hello");
 
-    for (var pingpong = ;)  {
-      console.log("pingpong");
-    }
+    // if (startingnumber % 3 === 0) {
+    //    console.log("ping");
+    // } else if (startingnumber % 5 ===0) {
+    //    console.log("pong");
+    // } else {
+    //   startingnumber % 3 === 0 && startingnumber % 5 === 0 {
+    //     console.log("ping-pong");
+    //   }
+    // }
 
-    });
+    // for (var ping = startingnumber % 3 === 0; ) {
+    //   console.log("ping");
+    // }
+    // for (var pong = ;){
+    //   console.log("pong");
+    // }
+    //
+    // for (var pingpong = ;)  {
+    //   console.log("pingpong");
+    // }
+    //
+    // });
 
+//     if (startingnumber % 3 === 0) {
+//     console.log("ping");
+// }
       $("#result").text(result);
     //need to increment by 1 so need to create an array
 
+    });
   });
