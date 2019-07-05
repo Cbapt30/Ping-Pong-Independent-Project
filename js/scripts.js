77b8809ca6d8c55b814ae0e25dca7f2d553b11ee
Celia Baptiste <cfbaptiste@gmail.com>
2019-07-04 21:40:21 -0700

    tweek business logic some more

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
 // Business Logic
 // handles core functionality and computations of the application
 
+var result = [];
 var pingPongCaculator =function()  {
-  if (inputnumber % 3 === 0)
-  return ping;
-else if (inputnumber % 5 === 0)
-  return pong;
-else if (inputnumber % 15 ===0)
-  return pingPong;
- } else if {
-return startingNumber;
+  if (inputNumber % 3 === 0)
+  result.push('ping');
+  else if (inputNumber % 5 === 0)
+  result.push('pong');
+  else if (inputNumber % 15 ===0)
+  result.push('pingPong');
+  else if (inputNumber % 3 !=== 0 && inputNumber % 5!===0 && inputNumber % !===15)
+  result.push(i);
  }
 };
-        
-var pingpongArraycalc = [];
-var pingPong = function(number) {
+//         console.log("help");
+// var pingpongArraycalc = [];
+// var pingPong = function(number) {
   
-};
+// };
 
 
 $(document).ready(function()  {
   $("form#ping-pong").submit(function(event)  {
     event.preventDefault();
-    var number = parseInt($("#inputnumber").val());
+    
+    var number = parseInt($("#inputNumber").val());
  
 
-    for (var startingNumber = 0; startinNnumber <= number; startingNumber +=1)  {
-    console.log(startingNumber);
-    event.preventDefault();
+    for (var i=0; i<= number; i+=1)  {
+      console.log('i is', i);
     }
+      
+    
+
   });
  
