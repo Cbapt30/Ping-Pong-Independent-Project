e18ff42eee9c13e5405bc96fd2c5129680d4a08d
Celia Baptiste <cfbaptiste@gmail.com>
2019-07-06 18:18:02 -0700

    update business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,37 @@
 
 var result = [];
 var pingPongCaculator =function()  {
-  if (inputNumber % 3 === 0)
-  result.push('ping');
-  else if (inputNumber % 5 === 0)
-  result.push('pong');
-  else if (inputNumber % 15 ===0)
-  result.push('pingPong');
-  else if (inputNumber % 3 !=== 0 && inputNumber % 5!===0 && inputNumber % !===15)
-  result.push(i);
- }
+// var number = parseInt(#blanks("#inputNumber").val());
+ 
+  if (inputNumber % 3 === 0)  {
+    result.push('ping');
+  } else if (inputNumber % 5 === 0) {
+    result.push('pong');
+  } else if (inputNumber % 15 === 0) {
+    result.push('pingPong');
+  } else if (result !== 'ping' || result !== 'pong' && result !== 'pingPong') {
+    result.push(i);
+  } else  {
+    alert('Please enter a valid number');
+  }  
 };
+
 //         console.log("help");
 // var pingpongArraycalc = [];
 // var pingPong = function(number) {
   
 // };
-
+//User Interface Logic
 
 $(document).ready(function()  {
   $("form#ping-pong").submit(function(event)  {
     event.preventDefault();
-    
+
     var number = parseInt($("#inputNumber").val());
  
 
     for (var i=0; i<= number; i+=1)  {
-      console.log('i is', i);
+      console.log('i');
     }
       
     
