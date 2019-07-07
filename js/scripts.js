87ebc02bce55c550120dc264e29d14b894b70c1a
Celia Baptiste <cfbaptiste@gmail.com>
2019-07-06 23:07:33 -0700

    change things around in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,50 @@
 // Business Logic
 // handles core functionality and computations of the application
 
-var result = [];
-var pingPongCaculator =function()  {
-// var number = parseInt(#blanks("#inputNumber").val());
+
+  // var number = parseInt($("#inputNumber").val());
  
-  if (inputNumber % 3 === 0)  {
-    result.push('ping');
-  } else if (inputNumber % 5 === 0) {
-    result.push('pong');
-  } else if (inputNumber % 15 === 0) {
-    result.push('pingPong');
-  } else if (result !== 'ping' || result !== 'pong' && result !== 'pingPong') {
-    result.push(i);
+  // var results = [];
+
+ 
+  // var pingPongCalculator = [];
+
+
+console.log('hello');
+var pingPongCalculator =function(number) {
+  if (i % 3 === 0)  {
+    results.push('ping');
+  } else if (i % 5 === 0) {
+    results.push('pong');
+  } else if (i % 15 === 0) {
+    results.push('pingPong');
+  } else if (i !== 'ping' || i !== 'pong' && i !== 'pingPong') {
+    results.push('i');
   } else  {
     alert('Please enter a valid number');
   }  
 };
 
-//         console.log("help");
-// var pingpongArraycalc = [];
-// var pingPong = function(number) {
-  
-// };
+var number = parseInt($("#inputNumber").val());
+
+
+
+
+
 //User Interface Logic
 
 $(document).ready(function()  {
   $("form#ping-pong").submit(function(event)  {
     event.preventDefault();
-
-    var number = parseInt($("#inputNumber").val());
- 
+    var results = [];
+    // var result= pingPongCalculator(number);
+    // pingPongCalculator(number);
 
     for (var i=0; i<= number; i+=1)  {
-      console.log('i');
+      results.push('i');
     }
       
+    $("#output").show(results);
     
-
-  });
- 
+      });
+    });     
